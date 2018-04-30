c09349731683fcc127a790024723d00edff3ae2d
Jeison Cardona Gómez <jeison.cardona@correounivalle.edu.co>
2018-04-29 22:08:53 -0500

    + FIXED missing initialization.

--- src/game.cpp
+++ src/game.cpp
@@ -35,9 +35,13 @@ int Game::minmax( Board board, bool pc_turn ){
     if( pc_turn ){
         current_max_position[0] = pos_pc[0];
         current_max_position[1] = pos_pc[1];
-    }else{
         current_min_position[0] = pos_human[0];
         current_min_position[1] = pos_human[1];
+    }else{
+        current_max_position[0] = pos_human[0];
+        current_max_position[1] = pos_human[1];
+        current_min_position[0] = pos_pc[0];
+        current_min_position[1] = pos_pc[1];
     };
 
     struct minmax_game_elements {
